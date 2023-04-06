d4f6ee212b27927f64af91d74f9be8cecf73d0d4
sora <sorazora1.4@gmail.com>
2023-04-06 06:55:58 -0700

    changed the home page and added a custom background image with the title for the home page

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
-import '@/styles/globals.css'
+import React from "react";
+
+import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <React.Fragment>
+      <Component {...pageProps} />
+      <footer>
+        <p>© 2023 Sora</p>
+      </footer>
+    </React.Fragment>
+  );
 }
