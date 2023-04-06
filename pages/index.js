f43d8b87bba5d94cf4b4b42caf2b18517dbf3105
sora <sorazora1.4@gmail.com>
2023-04-06 08:18:01 -0700

    added background hero image and styled it by adjusting the position and z index

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,14 @@
-import React from 'react'
-import Head from 'next/head'
+import React from "react";
+import Head from "next/head";
+import Image from "next/image";
 
-import Banner from '@/components/banner'
+import Banner from "@/components/banner";
 
-import styles from '@/styles/Home.module.css'
+import styles from "@/styles/Home.module.css";
 
 export default function Home() {
   const searchShopsHandler = () => {
-    console.log('button was clicked');
+    console.log("button was clicked");
   };
 
   return (
@@ -15,13 +16,19 @@ export default function Home() {
       <div className={styles.container}>
         <Head>
           <title>Coffee Connoisseur</title>
-          <link rel='icon' href='/favicon.ico' />
+          <link rel="icon" href="/favicon.ico" />
         </Head>
 
         <main className={styles.main}>
-          <Banner buttonText={"View Stores Nearby"} searchShops={searchShopsHandler} />
+          <Banner
+            buttonText={"View Stores Nearby"}
+            searchShops={searchShopsHandler}
+          />
+          <div className={styles.heroImage}>
+            <Image src='/static/hero-image.png' width={700} height={400} alt="hero image" />
+          </div>
         </main>
       </div>
     </React.Fragment>
   );
-};
+}
