1af565b79cdb9f442196852a7a2c13163ea018f0
sora <sorazora1.4@gmail.com>
2023-04-06 20:24:14 -0700

    moved the fetch api call to a separate file, fetch-coffeeStores

--- pages/index.js
+++ pages/index.js
@@ -4,29 +4,17 @@ import Image from "next/image";
 
 import Banner from "@/components/banner";
 import Card from "@/components/card";
-import coffeeStoresJSON from "../data/coffee-stores.json";
+import { fetchCoffeeStores } from "@/lib/fetch-coffeeStores";
 
 import styles from "@/styles/Home.module.css";
 
 export async function getStaticProps(context) {
-  const options = {
-    method: "GET",
-    headers: {
-      Accept: "application/json",
-      Authorization: "fsq3e73jZKnoWFKrvpwbHVZGc9a3s9TNcPARsBqBIv/vqeM=",
-    },
-  };
-
-  const response = await fetch(
-    "https://api.foursquare.com/v3/places/search?query=coffee&ll=43.653833032607096%2C-79.37896808855945&limit=6",
-    options
-  );
-
-  const data = await response.json();
+  
+  const coffeeStores = await fetchCoffeeStores();
 
   return {
     props: {
-      coffeeStores: data.results,
+      coffeeStores,
     },
   };
 }
