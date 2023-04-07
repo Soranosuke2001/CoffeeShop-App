4201333afeeb072aedfd05850e2590feabeace56
sora <sorazora1.4@gmail.com>
2023-04-07 08:44:30 -0700

    organized the code and exported some code to a external function

--- pages/api/createCoffeeStore.js
+++ pages/api/createCoffeeStore.js
@@ -1,9 +1,4 @@
-const Airtable = require("airtable");
-const base = new Airtable({
-  apiKey: process.env.AIRTABLE_API_KEY,
-}).base(process.env.AIRTABLE_BASE_KEY);
-
-const table = base("Coffee-Stores");
+import { getRecords, table } from "@/lib/airtable";
 
 const createCoffeeStore = async (req, res) => {
   if (req.method === "POST") {
@@ -17,52 +12,43 @@ const createCoffeeStore = async (req, res) => {
       imageURL,
     } = req.body;
     try {
-        if (storeId) {
+      if (storeId) {
+        const findRecord = await table
+          .select({
+            filterByFormula: `storeId=${storeId}`,
+          })
+          .firstPage();
 
-            const findRecord = await table
-              .select({
-                filterByFormula: `storeId=${storeId}`,
-              })
-              .firstPage();
-      
-            if (findRecord.length !== 0) {
-              const CoffeeShop = findRecord.map((record) => {
-                return {
-                  ...record.fields,
-                };
-              });
-              res.json(CoffeeShop);
-            } else {
-              if (name) {
-                const newRecord = await table.create([
-                  {
-                    fields: {
-                      storeId,
-                      name,
-                      formatted_address,
-                      region,
-                      locality,
-                      voting,
-                      imageURL,
-                    },
-                  },
-                ]);
-                const newShop = newRecord.map((record) => {
-                  return {
-                    ...record.fields,
-                  };
-                });
-                res.status(200);
-                res.json({ record: newShop });
-              } else {
-                res.status(422);
-                res.json({ message: "The name is required" });
-              }
-            }
+        if (findRecord.length !== 0) {
+          const coffeeShop = getRecords(findRecord);
+          res.json(coffeeShop);
         } else {
+          if (name) {
+            const newRecord = await table.create([
+              {
+                fields: {
+                  storeId,
+                  name,
+                  formatted_address,
+                  region,
+                  locality,
+                  voting,
+                  imageURL,
+                },
+              },
+            ]);
+            const newShop = getRecords(newRecord);
+            res.status(200);
+            res.json({ record: newShop });
+          } else {
             res.status(422);
-            res.json({ message: "The storeId is required" });
+            res.json({ message: "The name is required" });
+          }
         }
+      } else {
+        res.status(422);
+        res.json({ message: "The storeId is required" });
+      }
     } catch (error) {
       console.log("There was an error creating or finding a store: ", error);
       res.status(500);
