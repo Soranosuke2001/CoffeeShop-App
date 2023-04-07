b4839f4334d8e2f803091608ff1ff83ccde62e80
sora <sorazora1.4@gmail.com>
2023-04-07 07:37:45 -0700

    connected to the airtable database

---
+++ pages/api/createCoffeeStore.js
@@ -0,0 +1,16 @@
+const Airtable = require('airtable');
+const base = new Airtable({
+    apiKey: process.env.AIRTABLE_API_KEY
+}).base(
+    process.env.AIRTABLE_BASE_KEY
+);
+
+const table = base('Coffee-Stores');
+
+console.log(table)
+
+const createCoffeeStore = (req, res) => {
+    res.json({ message: 'this is working' })
+};
+
+export default createCoffeeStore;
