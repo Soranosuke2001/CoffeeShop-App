d8190f43764f2c32151573f4987d42ae614abff2
Sora <sorazora1.4@gmail.com>
2023-04-07 04:37:12 -0700

    added react context feature and setup its initial settings in _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,54 @@
-import React from "react";
+import React, { createContext } from "react";
 
 import "@/styles/globals.css";
 
+const storeContext = createContext();
+
+const ACTION_TYPES = {
+  SET_LAT_LONG: 'SET_LAT_LONG',
+  SET_COFFEE_STORES: 'SET_COFFEE_STORES'
+};
+
+const storeReducer = (state, action) => {
+  switch (action.type) {
+    case ACTION_TYPES.SET_LAT_LONG: {
+      return {
+        ...state,
+        latLong: action.payload.latLong
+      };
+    }
+
+    case ACTION_TYPES.SET_COFFEE_STORES: {
+      return {
+        ...latLong,
+        coffeeStores: action.payload.coffeeStores
+      };
+    }
+
+    default:
+      throw new Error(`Unknown action ${action.type}`);
+  };
+};
+
+const StoreProvider = ({ children }) => {
+  const initialState = {
+    latLong: "",
+    coffeeStores: []
+  };
+
+  const [state, dispatch] = useReducer(storeReducer, initialState);
+
+  return (
+    <storeContext.Provider value={{ state, dispatch }}>
+      {children}
+    </storeContext.Provider>
+  );
+};
+
 export default function App({ Component, pageProps }) {
   return (
-    <React.Fragment>
+    <StoreProvider>
       <Component {...pageProps} />
-    </React.Fragment>
+    </StoreProvider>
   );
-}
+};
