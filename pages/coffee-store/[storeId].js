3cb133f9d771aa1158e2039f162ef26235d1544a
sora <sorazora1.4@gmail.com>
2023-04-07 22:33:57 -0700

    added a max radius when searching for nearby coffee stores

--- pages/coffee-store/[storeId].js
+++ pages/coffee-store/[storeId].js
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import Link from "next/link";
 import Head from "next/head";
 import Image from "next/image";
