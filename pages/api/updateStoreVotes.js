5c66404ca4f95792d3b95a53abb1155b2dc7c218
sora <sorazora1.4@gmail.com>
2023-04-07 21:24:50 -0700

    updates the voting count by 1 in the database

--- pages/api/updateStoreVotes.js
+++ pages/api/updateStoreVotes.js
@@ -1,4 +1,4 @@
-import { searchCoffeeStore } from "@/lib/airtable";
+import { searchCoffeeStore, table } from "@/lib/airtable";
 
 const updateStoreVote = async (req, res) => {
     if (req.method === 'PUT') {
@@ -7,9 +7,27 @@ const updateStoreVote = async (req, res) => {
 
             if (storeId) {
                 const records = await searchCoffeeStore(storeId);
-    
+                const { recordId } = records[0];
+
                 if (records.length !== 0) {
-                    res.json(records);
+                    const newVote = parseInt(records[0].voting) + 1;
+                    console.log(newVote);
+
+                    const updateRecord = await table.update([
+                        {
+                            id: recordId,
+                            fields: {
+                                voting: newVote
+                            }
+                        }
+                    ]);
+
+                    if (updateRecord) {
+                        res.json({ updateRecord })
+                    } else {
+                        res.status(500);
+                        res.json({ message: 'There was an error updating the vote count' })
+                    }
                 } else {
                     res.json({ message: `Coffee storeId: ${storeId}, could not be found` });
                 };
